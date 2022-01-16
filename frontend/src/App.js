612f37d358bd783e6d9b82ba9bf173e4d4dd3a99
nbowd <bowdenn@oregonstate.edu>
2022-01-15 21:12:51 -0800

    User info mobile layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,65 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Welcome from './components/Welcome';
 import SearchControls from './components/SearchControls';
 import CreateCards from './components/CreateCards';
 import Login from './components/Login';
-import jwt_decode from "jwt-decode";
-
-
+import jwt_decode from 'jwt-decode';
 
 function App() {
-    const [results, setResults] = useState(null)
-    const [email, setEmail] = useState('') 
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [user, setUser] = useState(null)
-    const [token, setToken] = useState(null)
-    
-    // TRY TO MOVE THIS TO LOGIN COMPONENT
-    const loginProps = {email, setEmail, username, setUsername, password, setPassword, setUser, setToken, user, setResults}
+  const [results, setResults] = useState(null);
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
 
-    useEffect(() => {
-        const loggedUserJSON = window.localStorage.getItem('loggedBookappUser')
-        
-        if (!loggedUserJSON) return null
-        
-        const user = JSON.parse(loggedUserJSON)
-        
-        let decodedToken = jwt_decode(user.token);
-        let currentDate = new Date();
-        // Checks that token is still valid 
-        if (decodedToken.exp * 1000 > currentDate.getTime()) {
-            setUser(user)
-            setToken(user.token)
-        }
-    }, [])
- 
-    return <div id='app'>
-        <Login props={loginProps} />
-        <div className="intro">
-            <h1 className="title">Booknook</h1>
-            <SearchControls setResults={setResults} user={user} />    
-        </div>
+  // TRY TO MOVE THIS TO LOGIN COMPONENT
+  const loginProps = {
+    email,
+    setEmail,
+    username,
+    setUsername,
+    password,
+    setPassword,
+    setUser,
+    setToken,
+    user,
+    setResults,
+  };
 
-        {/* Search results displayed as cards, instructions displayed if no subject has been searched yet */}
-        {results?
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBookappUser');
+
+    if (!loggedUserJSON) return null;
+
+    const user = JSON.parse(loggedUserJSON);
+
+    let decodedToken = jwt_decode(user.token);
+    let currentDate = new Date();
+    // Checks that token is still valid
+    if (decodedToken.exp * 1000 > currentDate.getTime()) {
+      setUser(user);
+      setToken(user.token);
+    }
+  }, []);
+
+  return (
+    <div id="app">
+      <div className="intro">
+        <h1 className="title">Booknook</h1>
+        <SearchControls setResults={setResults} user={user} />
+      </div>
+      <Login props={loginProps} />
+
+      {/* Search results displayed as cards, instructions displayed if no subject has been searched yet */}
+      {results ? (
         <CreateCards token={token} results={results} setResults={setResults} />
-        : 
-        <Welcome/>}
+      ) : (
+        <Welcome />
+      )}
     </div>
+  );
 }
 
 export default App;
