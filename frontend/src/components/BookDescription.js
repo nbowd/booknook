cc1a5b2894d82ea135838f486e7a94fa3b3ab060
nbowd <bowdenn@oregonstate.edu>
2022-01-16 14:10:38 -0800

    Rough draft complete mobile layout

--- frontend/src/components/BookDescription.js
+++ frontend/src/components/BookDescription.js
@@ -1,29 +1,36 @@
-import React from 'react'
-import './BookDescription.css'
+import React from 'react';
+import './BookDescription.css';
 import TextareaAutosize from 'react-autosize-textarea';
 
+function BookDescription({ description }) {
+  return (
+    <div className="desc">
+      <div className="description__heading">
+        <h4>Description:</h4>
+      </div>
 
-function BookDescription({description}) {
-    return (
-        <div className='desc'>
-            <div className="description__heading"><h4>Description:</h4></div>
-
-            <div style={{display:'table'}}>
-                { description? 
-                <div style={{display: 'table-cell', verticalAlign: 'middle'}}>
-                    <TextareaAutosize style={{'maxHeight': '1000px'}} className="description" rows={2} maxRows={15} readOnly value={description}/>
-                </div>
-                : 
-                <>
-                    <div className="skeleton skeleton-text"></div>
-                    <div className="skeleton skeleton-text"></div>
-                    <div className="skeleton skeleton-text"></div>
-                    <div className="skeleton skeleton-text"></div>
-                </>
-                }
-            </div>
-        </div>
-    )
+      <div style={{ display: 'table' }}>
+        {description ? (
+          <div style={{ display: 'table-cell', verticalAlign: 'middle' }}>
+            <TextareaAutosize
+              className="description"
+              rows={2}
+              maxRows={15}
+              readOnly
+              value={description}
+            />
+          </div>
+        ) : (
+          <>
+            <div className="skeleton skeleton-text"></div>
+            <div className="skeleton skeleton-text"></div>
+            <div className="skeleton skeleton-text"></div>
+            <div className="skeleton skeleton-text"></div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default BookDescription
+export default BookDescription;
