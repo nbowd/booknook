6d2fa7dce7c97c55bd7cce7758181350f0d724b7
nbowd <bowdenn@oregonstate.edu>
2022-01-15 21:38:31 -0800

    Welcome screen mobile layout

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,27 +1,30 @@
-import React from 'react'
-import './Welcome.css'
+import React from 'react';
+import './Welcome.css';
 
 function Welcome() {
-    return (
-        <div className='welcome'>
-            <div>
-                <p>Welcome to Booknook!</p>
-                <p>Get unique book recommendations for dozens of subjects</p>
-            </div>
-            <div className='get-started'>
-                <p>Getting Started:</p>
-                <ul>
-                    <li>Type or scroll through the subjects list</li>
-                    <li>Click 'Find Books' to get results</li>
-                    <li>Click 'More Info' to see a book description, shopping link, and save options</li>
-                </ul>
-            </div>
-            <div>
-                Register an account to save a book for later. While logged in, click 'Saved Books' to view/delete the user's saved selections.
-            </div>
-  
-        </div>
-    )
+  return (
+    <div className="welcome">
+      <div>
+        <h3 className="welcome-title">Welcome to Booknook!</h3>
+        <p>Unique book recommendations for dozens of subjects</p>
+      </div>
+      <div className="get-started">
+        <p>Getting Started:</p>
+        <ul>
+          <li>Type or scroll through the subjects list</li>
+          <li>Click 'Find Books' to get results</li>
+          <li>
+            Click 'More Info' to see a book description, shopping link, and save
+            options
+          </li>
+        </ul>
+      </div>
+      <div>
+        Register an account to save a book for later. While logged in, click
+        'Saved Books' to view/delete the user's saved selections.
+      </div>
+    </div>
+  );
 }
 
-export default Welcome
+export default Welcome;
